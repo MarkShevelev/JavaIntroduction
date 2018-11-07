bf9d4ea1fe4215cea8555409313f242686f69b24
MarkShevelev <shevelev_m@itstep.org>
2018-11-07 12:25:57 +0300

    Memoization with arrays examples added

--- ArraysExamples/src/org/itstep/ArraysExamples.java
+++ ArraysExamples/src/org/itstep/ArraysExamples.java
@@ -1,5 +1,6 @@
 package org.itstep;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArraysExamples {
@@ -153,6 +154,81 @@ public class ArraysExamples {
                     base[pos] = transform[(base[pos]+base[pos+1])%3];
 
             System.out.println(chars[base[0]]);
+        }
+
+        /**
+         * Массивы можно использовать для решения некоторых задач методом динамического программирования
+         * Задача
+         * Найти N-e число Фибоначчи, заданное рекурентно: Fib(N) = Fib(N-1) + Fib(N-2), Fib(1)=1, Fib(2)=1
+         * */
+        if (false) {
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            long[] fibonacci = new long[N];
+            fibonacci[0] = 1; fibonacci[1] = 1;
+
+            for (int pos = 2; pos < N; ++pos)
+                fibonacci[pos] = fibonacci[pos-1]+fibonacci[pos-2];
+
+            System.out.println(fibonacci[N-1]);
+        }
+
+        /**
+         * Задача
+         * Между двумя берегами реки есть сухой путь по камушкам
+         * На каждом камушке лежит некоторое неотрицательное количество конфет
+         * На одном берегу сидит зайчик, он может прыгать между камушками либо через один камень, либо через два,
+         * но не может прыгнуть на следующий камень или через три камня, или назад
+         * Зная количество конфет на каждом камушке, найдите наибольшее возможное количество конфет, которое соберёт
+         * зайчик, прыгая с одного берега реки на другой
+         * */
+        if (false) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите количество камушков не менее пяти: ");
+            int numberOfStones = sc.nextInt();
+
+            System.out.println("Введите количество конфет на каждом камушке: ");
+            int[] sweets = new int[numberOfStones];
+
+            for (int pos=0; pos != numberOfStones; ++pos)
+                sweets[pos] = sc.nextInt();
+
+
+            //в этом массиве запоминаем максимально возможное количество конфет, которое соберёт зайчик, если окажется
+            //на камне с определённым индексом
+            long[] sweetsCollected = new long[numberOfStones];
+            //так, на первый камень зайчик не может попасть
+            sweetsCollected[0] = 0;
+            //если он попадёт на второй камень, то соберёт ровно столько, сколько там лежит
+            sweetsCollected[1] = sweets[1];
+            //если он попадёт на третий камень, то также соберёт ровно столько, сколько там лежит,
+            //т.к. есть единственный способ туда попасть
+            sweetsCollected[2] = sweets[2];
+
+            //на четвёртый камень можно попасть только со второго, совершив два прыжка через один камень
+            // s| 0 1 2 3 : s->1->3
+            sweetsCollected[3] = sweets[3]+sweetsCollected[1];
+
+            //однако на пятый камень можно попасть разными путями:
+            // s| 0 1 2 3 4
+            // s->1->4  через один и через два
+            // s->2->4  через два и через один
+            //Необходимо выбрать тот путь, который даст наибольшее количество конфет
+            sweetsCollected[4] = sweets[4] + (sweetsCollected[2] > sweetsCollected[1] ? sweetsCollected[2] : sweetsCollected[1]);
+
+            //Для любого номера островка есть два пути туда попасть
+            // N-2 -> N прыжок через один
+            // N-3 -> N прыжок через два
+            for (int pos = 5; pos != numberOfStones; ++pos)
+                sweetsCollected[pos] = sweets[pos] +
+                        (sweetsCollected[pos-2] > sweetsCollected[pos-3] ? sweetsCollected[pos-2] : sweetsCollected[pos-3]);
+
+            //Зайчик может закончить путь либо на последнем, либо на предпоследнем островке
+            //(находясь на предпоследнем островке, он уже не может забрать конфеты с последнего),
+            //полный результат - максимум из конфет, набранных к последнему и предпоследнему островку
+            long res = (sweetsCollected[numberOfStones-1] > sweetsCollected[numberOfStones-2] ? sweetsCollected[numberOfStones-1] : sweetsCollected[numberOfStones-2]);
+            System.out.println("Наибольшее число конфет: " + res);
+            System.out.println(Arrays.toString(sweetsCollected));
         }
     }
 }
