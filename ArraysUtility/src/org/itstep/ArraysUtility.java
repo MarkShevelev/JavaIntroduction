92d54b8eb7591489afa341438820d1ce5b326541
MarkShevelev <shevelev_m@itstep.org>
2018-11-07 12:53:19 +0300

    Arrays utility started

---
+++ ArraysUtility/src/org/itstep/ArraysUtility.java
@@ -0,0 +1,10 @@
+package org.itstep;
+
+public class ArraysUtility {
+    public static void main(String[] args) {
+        /**
+         * Стандартная библиотека Java включает в себя большой набор стандартных средств для работы с массивами
+         * Значительная их часть представлена статическими методами класса Arrays
+         * */
+    }
+}
